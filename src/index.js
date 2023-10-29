55b02fc517d7a28dd356d40b80c45efde063f7e9
Vasashock <gamergoway@gmail.com>
2023-10-29 19:23:00 +0300

    test passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let nS = n.toString();
+  let result = '';
+  for (let i = nS.length - 1  ; i >= 0; i--) {
+    const element = nS[i];
+    console.log(nS[i]);
+    result += nS[i];
+  }
+  return Number(result.replace('-',''));
 }
