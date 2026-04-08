4001c995a5bf72f18a45daa401e392190f6473f1
Vasashock <gamergoway@gmail.com>
2026-04-08 07:58:26 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const nS = n.toString();
+  let result = '';
+  for (let i = nS.length - 1; i >= 0; i--) {
+    result += nS[i];
+  }
+  return Number(result.replace('-', ''));
 };
